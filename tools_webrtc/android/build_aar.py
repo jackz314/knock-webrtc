a0c210baa05240c70d28d5ab438f8f273c39fec2
Jack Zhang <zhangmengyu10@gmail.com>
2019-04-06 15:12:03 -0700

    fixed python build script logging

--- tools_webrtc/android/build_aar.py
+++ tools_webrtc/android/build_aar.py
@@ -223,7 +223,7 @@ def BuildAar(archs, output_file, use_goma=False, extra_gn_args=None,
 
 
 def main():
-  sys.stdout.write("Starting aar build...");
+  sys.stdout.write("Starting aar build...\n");
   args = _ParseArgs()
   logging.getLogger().setLevel(logging.DEBUG if args.verbose else logging.INFO)
   logging.basicConfig(stream=sys.stdout, level=logging.DEBUG if args.verbose else logging.INFO)
