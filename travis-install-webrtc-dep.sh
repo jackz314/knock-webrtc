732ac576d0aedd243f35d1a6df5f9d722370902e
Jack Zhang <zhangmengyu10@gmail.com>
2019-04-06 19:23:44 -0700

    fixed problems

--- travis-install-webrtc-dep.sh
+++ travis-install-webrtc-dep.sh
@@ -6,7 +6,7 @@ git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=$PATH:$HOME/webrtc-dep/depot_tools
 
 #gclient stuff
-mv `pwd` ../src #rename the current directory (repo root) to src so all the scripts work without problem
-sed -i -e "s|'src/resources'],|'src/resources'],'condition':'rtc_include_tests==true',|" DEPS # not get resources when gclient sync
 mv .gclient ../.gclient #move gclient to parent directory to use as config
+sed -i -e "s|'src/resources'],|'src/resources'],'condition':'rtc_include_tests==true',|" DEPS # not get resources when gclient sync
+mv `pwd` ../src #rename the current directory (repo root) to src so all the scripts work without problem
 #not doing gclient sync directly here since doing it individually provides more insight and error log
