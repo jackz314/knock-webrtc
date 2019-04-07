7c3168e8470666b04451b784b9e6253217e9863e
Jack Zhang <zhangmengyu10@gmail.com>
2019-04-07 01:57:04 -0700

    fixed tagging problem

---
+++ get-build-num.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# starts with correct tag info, increment build number
+LAST_TAG=$(git describe --tags --always)
+if [ $LAST_TAG==AutoBuild-* ]
+then
+    NEW_NUM=$((${LAST_TAG:10}+1)) #only keep the number and increment
+    echo $NEW_NUM
+else #start a new build number
+    echo 1
+fi
