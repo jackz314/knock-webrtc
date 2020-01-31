4a903d4e7ecfec816c9bb3f7e44680e0ce62ebf0
Jack Zhang <zhangmengyu10@gmail.com>
2020-01-31 00:52:26 -0800

    Add maven git user info, fix knock-webrtc release stuff

--- publish-maven.sh
+++ publish-maven.sh
@@ -5,6 +5,8 @@ mkdir maven
 cd maven
 git clone https://$GITHUB_TOKEN@github.com/jackz314/jackz314-maven.git
 cd jackz314-maven
+git config --local user.name "Jack Zhang"
+git config --local user.email "zhangmengyu10@gmail.com"
 git checkout knock-webrtc
 mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -DgroupId=com.jackz314 -DartifactId=knock-webrtc -Dversion=1.0.$buildNum -DlocalRepositoryPath=./ -Dpackaging=aar -Dfile=../../lib-knock-webrtc.aar -DgeneratePom=true -DcreateChecksum=true
 git add -A #stage changes
